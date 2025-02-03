6512ba290e908d200b821b662394849811fffbb6
Norisuke1028 <138739405+Norisuke1028@users.noreply.github.com>
2025-02-03 13:07:12 +0900

    土管クラス編集
    土管クラス編集中
    当たり判定を書いてる途中

--- Develop/Dokan.cpp
+++ Develop/Dokan.cpp
@@ -1,1 +1,33 @@
 #include "Dokan.h"
+#include "DxLib.h"
+
+Dokan::Dokan()
+{
+
+}
+
+Dokan::~Dokan()
+{
+
+}
+
+void Dokan::Initialize()
+{
+	// 当たり判定の設定
+	box_size = 32.0f;
+}
+
+void Dokan::Draw(const Vector2D& screen_offset) const
+{
+
+}
+
+void Dokan::Finalize()
+{
+
+}
+
+void Dokan::OnHitCollision(GameObject* hit_object)
+{
+	//もし、4つ目の土管に入ったら地下マップへ行く
+}
--- Develop/Dokan.h
+++ Develop/Dokan.h
@@ -1,8 +1,18 @@
 #pragma once
 
+#include "../GameObject.h"
+#include "../../Utility/StageData.h"
 
-
-class Dokan
+class Dokan : public GameObject
 {
+public:
+	Dokan();
+	~Dokan();
+
+	virtual void Initialize() override;
+	virtual void Draw(const Vector2D& screen_offset) const override;
+	virtual void Finalize() override;
+
+	virtual void OnHitCollision(GameObject* hit_object) ;
 };
 
