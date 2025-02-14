7e8ff39c0b9b2f2ff6ebd7cef458d5f681f49d63
T0655 <138738962+T0655@users.noreply.github.com>
2025-02-14 21:04:26 +0900

    言われていたものの追加

--- Develop/Objects/Enemy/nokonoko.h
+++ Develop/Objects/Enemy/nokonoko.h
@@ -3,8 +3,50 @@
 #include "../GameObject.h"
 #include "../../Utility/StageData.h"
 
-
-class nokonoko
+enum eNokonokoState
 {
+    N_WALK,   //歩く・通常状態
+    N_STEP,   //踏まれた状態
 };
 
+class nokonoko : public GameObject
+{
+private:
+    std::vector<int> move_animation;   //移動のアニメーション
+
+    //アニメーション用
+    const int animation_num[2] = { 0,1 };
+    int animation_count;
+    float animation_time;
+
+    static nokonoko* instance;
+
+    //向き
+    Vector2D direction;
+
+    nokonoko* Kuribo;
+
+public:
+
+    //クリボーの状態
+    eNokonokoState nokonoko_state;
+
+    nokonoko();
+    ~nokonoko();
+
+    virtual void Initialize() override;
+    virtual void Update(float delta_second) override;
+    virtual void Draw(const Vector2D& screen_offset) const override;
+    virtual void Finalize() override;
+
+    /*virtual void OnHitCollision(GameObject* hit_object) ;*/
+
+    void SetVelocity(float velo);
+
+    static nokonoko* GetInstance();
+
+private:
+
+    void Movement(float delta_second);
+    void AnimeCount(float delta_second);
+};
--- Develop/Scene/InGameScene.cpp
+++ Develop/Scene/InGameScene.cpp
@@ -7,20 +7,19 @@
 
 #include "../Objects/Mario/Player.h"
 #include "../Objects/Enemy/kuribo.h"
+#include "../Objects/Enemy/nokonoko.h"
+
+#include "../Objects/Item/coin.h"
 
 #include "DxLib.h"
 #include <fstream>
 
 
 
-InGameScene::InGameScene()
-	:player(nullptr)
-	,Kuribo(nullptr)
-	,velocity(0.0f)
-	,fream_time(0.0f)
-	,mario_time(0.0f)
-	,mario_ui(0)
-	,mario_world(0)
+InGameScene::InGameScene() :
+	kakeru_ui(),
+	coin_num_min(),
+	coin_num_sec()
 {
 	back_ground_BGM = NULL;
 }
@@ -39,18 +38,30 @@ void InGameScene::Initialize()
 
 	StageData* Stage = StageData::GetInstance();
 	Stage->Load();
-	
+
+
+
 	//マリオの表示
 	player = CreateObject<Player>(Vector2D(100, 403));
 	//クリボーの表示
 	Kuribo = CreateObject<kuribo>(Vector2D(750, 403));
-	//genelate_location = Vector2D(700.0f, 528.0f);
+	Nokonoko = CreateObject<nokonoko>(Vector2D(350, 385));
+
+	//アニメーションしているコインの表示
+	Coin = CreateObject<coin>(Vector2D(175, 45));
+
 
 	//UIの読み込み
 	mario_ui = rm->GetImages("Resource/images/UI/name_mario.png")[0];
 	mario_time = rm->GetImages("Resource/images/UI/time.png")[0];
 	mario_world = rm->GetImages("Resource/images/UI/world.png")[0];
 
+	kakeru_ui = rm->GetImages("Resource/images/Ui/num.png", 14, 14, 1, 16, 16);
+
+	//同じ一枚の画像を入れる
+	//コイン取得数
+	coin_num_min = kakeru_ui;
+	coin_num_sec = kakeru_ui;
 }
 
 eSceneType InGameScene::Update(const float& delta_second)
@@ -77,33 +88,30 @@ eSceneType InGameScene::Update(const float& delta_second)
 
 		//クリボー用
 		Kuribo->SetVelocity(player->GetVelocity().x);
+		Nokonoko->SetVelocity(player->GetVelocity().x);
 	}
-
-
-
-
-	////終盤画面スクロールを止める
-	//if (scroll <= 1000)
-	//{
-	//	scroll = 1000;
-
-	//	player->SetScrollEnd();
-	//}
-
 
 	// 親クラスの更新処理を呼び出す
 	return __super::Update(delta_second);
 }
 
-void InGameScene::Draw() const 
+void InGameScene::Draw() const
 {
 	StageData* Stage = StageData::GetInstance();
 	Stage->Load();
+
+
 
 	//タイトルUIの描画
 	DrawGraph(340, 10, mario_world, TRUE);
 	DrawGraph(480, 10, mario_time, TRUE);
 	DrawGraph(100, 10, mario_ui, TRUE);
+
+	//コインの画像
+
+	DrawGraph(190, 33, kakeru_ui[11], TRUE);     //コイン横の×
+	DrawGraph(210, 34, coin_num_min[0], TRUE);   //コイン10の位の数字
+	DrawGraph(230, 34, coin_num_sec[0], TRUE);   //コイン１の位の数字
 
 	__super::Draw();
 
@@ -118,7 +126,55 @@ void InGameScene::Finalize()
 
 const eSceneType InGameScene::GetNowSceneType() const
 {
-	return eSceneType ::in_game;
+	return eSceneType::in_game;
 }
 
-
+//#ifdef D_PIVOT_CENTER
+//
+//
+////当たり判定チェック処理（矩形の中心で当たり判定をとる）
+//void InGameScene::HitCheckObject(GameObjectBase* a, GameObjectBase* b)
+//{
+//	//2つのオブジェクトの距離を取得
+//	Vector2D diff = a->GetLocation() - b->GetLocation();
+//
+//	//2つのオブジェクトの当たり判定の大きさを取得
+//	Vector2D box_size = (a->GetBoxSize() + b->GetBoxSize()) / 2.0f;
+//
+//	//距離より大きさが大きい場合、Hit判定とする
+//	if ((fabs(diff.x) < box_size.x) && (fabsf(diff.y) < box_size.y))
+//	{
+//		if (a->GetType() == b->GetType())
+//		{
+//			return;
+//		}
+//		else
+//		{
+//			//当たったことをオブジェクトに通知する
+//			a->OnHitCollision(b);
+//			b->OnHitCollision(a);
+//		}
+//	}
+//}
+//
+//#else
+//
+////当たり判定チェック処理（左上頂点の座標から当たり判定計算を行う)
+//void Scene::HitCheckObject(GameObjectBase* a, GameObjectBase* b)
+//{
+//	//右下頂点座標を取得する
+//	Vector2D a_lower_right = a->GetLocation() + a->GetBoxSize();
+//	Vector2D b_lower_right = b->GetLocation() + b->GetBoxSize();
+//
+//	//矩形Aと矩形Bの位置関係を調べる
+//	if ((a->GetLocation().x < b_lower_right.x) &&
+//		(a->GetLocation().y < b_lower_right.y) &&
+//		(a_lower_right.x > b->GetLocation().x) &&
+//		(a_lower_right.y > b->GetLocation().y))
+//	{
+//		//オブジェクトにたいしてHit判定を通知する
+//		a->OnHitCollision(b);
+//		b->OnHitCollision(a);
+//	}
+//}
+//#endif // D_PIVOT_CNETER
